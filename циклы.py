fc0c7e7b7b06d3835e7c3a0fd5c2d3d8f6b54749
Ovahin <y80741701@gmail.com>
2023-10-04 16:46:46 +0300

    Easy way to solve the task B

--- циклы.py
+++ циклы.py
@@ -7,9 +7,6 @@ for i in range(0, times):
 
 # Задача B:
 
-count = 0
 number = input('Введите число: ')
-for i in range(0, len(number)):
-    if number[i] == '1':
-        count += 1
+count = number.count('1')
 print(f'Единиц: {count}')
